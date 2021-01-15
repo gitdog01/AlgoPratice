5d328bd8ffb8cfc7a4ac8ac2702ba321845fb071
mononomo <hostdog01@naver.com>
2021-01-16 01:23:25 +0900

    2021-01-06 01

--- level_6/1065/main.py
+++ level_6/1065/main.py
@@ -1,3 +1,12 @@
 n = input()
+
 if (int(n) < 100):
-    return int(n)
+    print(n)
+elif int(n) == 1000:
+    print(144)
+else:
+    count = 99
+    for num in range(100, int(n)+1):
+        if int(str(num)[0])-int(str(num)[1]) == int(str(num)[1])-int(str(num)[2]):
+            count += 1
+    print(count)
